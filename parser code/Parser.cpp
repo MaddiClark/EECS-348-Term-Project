c2b2fe5dba82298839bd87013d6bf90d99c22020
MaddiClark <cclar@ku.edu>
2026-05-05 16:14:17 -0500

    Added sorting for tokens list

--- parser code/Parser.cpp
+++ parser code/Parser.cpp
@@ -3,9 +3,22 @@
 // Last edited by: Maddi Clark 5/5/26
 
 #include "Parser.h"
+#include <vector>
 
 Node* Parser::createTree() {
+    std::vector<Token> operatorStack[];
+    std::vector<Token> operandStack[];
 
+    for (int i = 0; i <= tokens.size; i++){ //sorts tokens into a list of operators and operands
+        if tokens[i].TokenType == NUMBER {
+            operandStack.push_back(tokens[i]);
+        }
+        else {
+            operatorStack.push_back(tokens[i]);
+        }
+    }
+
+    
 }
 
 int Parser::lowestPrecedenceOperator(int start, int end) {
