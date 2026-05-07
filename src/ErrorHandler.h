ad20cacc476a70c07075c67536746862b7fd2951
Jaycob Campos <121409303+RockettKings@users.noreply.github.com>
2026-05-07 09:46:45 -0500

    Added source code all in one spot

---
+++ src/ErrorHandler.h
@@ -0,0 +1,31 @@
+//
+// Created by Jaycob Campos on 4/17/26.
+// Updated by Tate Meyer on 5/5/26.
+//
+
+#ifndef PARSER_ERRORHANDLER_H
+#define PARSER_ERRORHANDLER_H
+#include <string>
+using namespace std;
+
+enum ErrorCodes {
+    INVALID_CHARACTER,
+    UNSUPPORTED_OPERATOR,
+    MISMATCHED_PARENTHESES,
+    MISSING_OPERAND,
+    DIVISION_BY_ZERO,
+    MISSING_OPERATOR,
+    UNMATCHED_PARENTHESES
+};
+
+class ErrorHandler {
+public:
+    ErrorHandler()= default;
+    void handler(ErrorCodes error, string cause, int position, string expression);
+
+private:
+    void highlightError(string expression, int position);
+};
+
+
+#endif //PARSER_ERRORHANDLER_H
